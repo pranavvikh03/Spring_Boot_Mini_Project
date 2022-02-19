88d3105f58138ac9f53c907b716d9160a3bcdf62
Hrithik Raj <hrithikraj@197NODMBT2749.local>
2022-02-19 07:17:42 +0530

    initial commit 🚀

---
+++ src/main/java/com/paytmmoney/sipmanagement/SipManagementApplication.java
@@ -0,0 +1,13 @@
+package com.paytmmoney.sipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paytmmoney/sipmanagement/SipManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paytmmoney.sipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
